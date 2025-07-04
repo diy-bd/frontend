5eab20bb8ef61d00036511495955e4c0982dae81
hrdipto <jhondipto@gmail.com>
2025-07-05 03:18:06 +0600

    update

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -1,20 +1,20 @@
 export const metadata = {
-  title: "Home - Open PRO",
-  description: "Page description",
+  title: 'Home - Open PRO',
+  description: 'Page description',
 };
 
-import PageIllustration from "@/components/page-illustration";
-import Hero from "@/components/hero-home";
-import Workflows from "@/components/workflows";
-import Features from "@/components/features";
-import Testimonials from "@/components/testimonials";
-import Cta from "@/components/cta";
+import PageIllustration from '@/components/page-illustration';
+import Hero from '@/components/hero-home';
+import Workflows from '@/components/workflows';
+import Features from '@/components/features';
+import Testimonials from '@/components/testimonials';
+import Cta from '@/components/cta';
 
 export default function Home() {
   return (
     <>
       <PageIllustration />
-      <Hero />
+      {/* <Hero /> */}
       <Workflows />
       <Features />
       <Testimonials />
--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -1,37 +1,38 @@
-"use client";
+'use client';
 
-import Link from "next/link";
-import Logo from "./logo";
+import Link from 'next/link';
+import Logo from './logo';
 
 export default function Header() {
   return (
-    <header className="z-30 mt-2 w-full md:mt-5">
-      <div className="mx-auto max-w-6xl px-4 sm:px-6">
-        <div className="relative flex h-14 items-center justify-between gap-3 rounded-2xl bg-gray-900/90 px-3 before:pointer-events-none before:absolute before:inset-0 before:rounded-[inherit] before:border before:border-transparent before:[background:linear-gradient(to_right,var(--color-gray-800),var(--color-gray-700),var(--color-gray-800))_border-box] before:[mask-composite:exclude_!important] before:[mask:linear-gradient(white_0_0)_padding-box,_linear-gradient(white_0_0)] after:absolute after:inset-0 after:-z-10 after:backdrop-blur-xs">
-          {/* Site branding */}
-          <div className="flex flex-1 items-center">
-            <Logo />
+    <header className="h-screen min-h-screen w-screen bg-white">
+      <div
+        className="relative h-screen flex align-center flex-col justify-center text-center bg-cover"
+        style={{
+          backgroundImage:
+            'url("https://images.unsplash.com/photo-1626761191814-a9dc9efd085c?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=2664&amp;q=80")',
+        }}
+      >
+        <span className="absolute top-0 left-0 w-full h-full bg-center bg-cover bg-gradient-to-tl from-gray-900 to-slate-800"></span>
+        <h1 className="block antialiased tracking-normal font-sans text-5xl font-semibold leading-tight text-white mb-4 !leading-tight lg:text-7xl z-20">
+          Check out our new collection
+        </h1>
+        <p className="block antialiased font-sans text-xl font-normal leading-relaxed text-white mb-12 w-full opacity-80 md:w-10/12 lg:w-7/12 mx-auto px-28 z-10">
+          The Basic Starter Pack allows you to fully express your vibrant
+          personality with four grayscale options. Feeling adventurous? Put on a
+          heather gray shirt. Want to be a trendsetter? Try our exclusive
+          colorway: `Orange`. Need to add an extra pop of color to your outfit?
+          Our white shirt has you covered.
+        </p>
+        <div className="w-auto mx-auto">
+          <div className="flex items-center">
+            <button
+              className="align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-sm py-3.5 px-7 rounded-lg bg-dark text-white w-full z-10"
+              type="button"
+            >
+              Show new arrival
+            </button>
           </div>
-
-          {/* Desktop sign in links */}
-          <ul className="flex flex-1 items-center justify-end gap-3">
-            <li>
-              <Link
-                href="/signin"
-                className="btn-sm relative bg-linear-to-b from-gray-800 to-gray-800/60 bg-[length:100%_100%] bg-[bottom] py-[5px] text-gray-300 before:pointer-events-none before:absolute before:inset-0 before:rounded-[inherit] before:border before:border-transparent before:[background:linear-gradient(to_right,var(--color-gray-800),var(--color-gray-700),var(--color-gray-800))_border-box] before:[mask-composite:exclude_!important] before:[mask:linear-gradient(white_0_0)_padding-box,_linear-gradient(white_0_0)] hover:bg-[length:100%_150%]"
-              >
-                Sign In
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="/signup"
-                className="btn-sm bg-linear-to-t from-indigo-600 to-indigo-500 bg-[length:100%_100%] bg-[bottom] py-[5px] text-white shadow-[inset_0px_1px_0px_0px_--theme(--color-white/.16)] hover:bg-[length:100%_150%]"
-              >
-                Register
-              </Link>
-            </li>
-          </ul>
         </div>
       </div>
     </header>
